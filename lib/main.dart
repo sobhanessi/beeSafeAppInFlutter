95299516017a0e49d952290c6edc779221085430
sobhanessi <sobhanessi@gmail.com>
2022-05-25 22:03:10 +0300

    just testing nothing more

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(
+    MaterialApp(
+      home: Home(),
+    )
+  );
 }
 
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (
+    Scaffold(
+      appBar: AppBar(
+        title : Text('Login Page'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: Text(
+          'Here is the main body',
+      ),
+    )
+    );
+  }
+}
