4e788f3e5bc63ee5ec43c5d76943dcf11855d756
sobhanessi <sobhanessi@gmail.com>
2022-06-02 09:21:06 +0300

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
+
+// dg kam kam bayad shoroo konam.
+
 void main() {
   runApp(
     MaterialApp(
@@ -17,23 +20,44 @@ class Home extends StatelessWidget {
     return (
     Scaffold(
       appBar: AppBar(
-        title : Text('Login Page'),
+        title : Text('Login Page',
+          style: TextStyle(color: Colors.black),
+        ),
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
-      body: Column(
-        children: [
-          SignInButton(
-              Buttons.Apple,
-              // text : 'Login from google',
+      body: Center(
+        child :
+
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // verticalDirection: VerticalDirection.down,
+          children: [
+            Image.asset("assets/images/logo.png"),
+            SignInButton(
+                Buttons.Apple,
+                // text : 'Login from google',
+                onPressed: (){
+                  print('I am clicked from google');
+                }),
+            SignInButton(Buttons.FacebookNew, onPressed: (){
+              print('i am clicked from facebook');
+            }),
+            SignInButton(Buttons.Google, onPressed: (){
+              print('i am clicked from google');
+            }),
+            SignInButton(
+              Buttons.Microsoft,
               onPressed: (){
-            print('I am clicked from google');
-          }),
-          SignInButton(Buttons.FacebookNew, onPressed: (){
-            print('i am clicked from facebook');
-          }),
-          SignInButton(Buttons.Google, onPressed: (){})
-      ]),
+                print('i am clicked from microsoft');
+              },
+
+            )
+          ],
+        ),
+      ),
+      backgroundColor: Colors.amber,
     )
     );
   }
